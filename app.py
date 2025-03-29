829a4246fc9d54e3b23d63178fd35673b73ccd7f
OLIVERC-2120\Admin <Sup3ri0r.mt2110@gmail.com>
2025-03-29 16:45:44 +0700

    Fix port issue

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
@@ -55,7 +56,8 @@ def reset_balance():
     socketio.emit('update', {"balance": balance.amount, "purchases": []})
     return redirect(url_for('index'))
 
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if not set
+    socketio.run(app, host='0.0.0.0', port=port)
+
